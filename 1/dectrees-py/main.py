2d239223a5c100e65bdc6685eb97e3d4dd767c7b
Hannes Leskelä <hleskela@kth.se>
2016-10-07 18:43:14 +0200

    Assignment 1, printing entropy of Monk1-3

--- 1/dectrees-py/main.py
+++ 1/dectrees-py/main.py
@@ -1,4 +1,9 @@
 import monkdata as m
+import dtree as dtree
 
-print("Hello, world!")
+print("Assignment 1:")
+print("Entropy of MONK-1:", dtree.entropy(m.monk1))
+print("Entropy of MONK-2:", dtree.entropy(m.monk2))
+print("Entropy of MONK-3:", dtree.entropy(m.monk3))
 
+print("Assignment 2:")
