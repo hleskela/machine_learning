91b6a603e3a4adf4023ad8c2ada8bf0604103053
Hannes Leskelä <hleskela@kth.se>
2016-10-07 20:47:43 +0200

    Add assignment 2, check entropy

--- 1/dectrees-py/main.py
+++ 1/dectrees-py/main.py
@@ -7,3 +7,7 @@ print("Entropy of MONK-2:", dtree.entropy(m.monk2))
 print("Entropy of MONK-3:", dtree.entropy(m.monk3))
 
 print("Assignment 2:")
+
+print("{:3} {:<23} {:<23} {:<23}".format("#", "MONK-1", "MONK-2", "MONK-3"))
+for at in m.attributes:
+    print("{:3} {:<23} {:<23} {:<23}".format(at.name, dtree.averageGain(m.monk1, at),dtree.averageGain(m.monk2, at),dtree.averageGain(m.monk3, at)))
